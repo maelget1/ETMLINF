2a87ad5aa88e8cd423fb4ee88bfc2736bfe6e4ad
maelget1 <112376326+maelget1@users.noreply.github.com>
2023-06-05 16:34:07 +0200

    start the pdo connection and his using

--- code/php/PDO.php
+++ code/php/PDO.php
@@ -1,11 +1,23 @@
 <?php
-// Se connecter via PDO
-try
-{
-    $connector = new PDO('mysql:host=localhost;dbname=etmlinf;charset=utf8' , 'root', 'root');
+class PDOconn{
+    function __construct()
+    {
+        // Se connecter via PDO
+        try
+        {
+            $connector = new PDO('mysql:host=localhost;dbname=etmlinf;charset=utf8' , 'root', 'root');
+        }
+        catch (PDOException $e)
+        {
+        die('Erreur : ' . $e->getMessage());
+        }
+    }
+
+    //TODO créer la requête de création de l'utilisateur
+    function createUser($username, $name, $firstname, $password, $mail)
+    {
+
+    }
 }
-catch (PDOException $e)
-{
-   die('Erreur : ' . $e->getMessage());
-}
+
 ?>
--- code/php/createUser.php
+++ code/php/createUser.php
@@ -1,4 +1,9 @@
 <?php
 include_once("PDO.php");
+$pdo = new PDOconn();
+
+//TODO créer la vérification du formulaire de création
+
+//$pdo->createUser();
 
 ?>
