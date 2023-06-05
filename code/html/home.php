fb5c7205de79098646403f0f9efb7aa17791d052
maelget1 <112376326+maelget1@users.noreply.github.com>
2023-06-05 14:41:00 +0200

    include some php in base files

---
+++ code/html/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php
+        include_once("header.html");
+    ?>
+
+    <main>
+        <h2>Welcome to Your Page</h2>
+        <p>This is the main content of your page.</p>
+    </main>
+
+    <?php
+    include_once("footer.html");
+    ?>
+
+</html>
