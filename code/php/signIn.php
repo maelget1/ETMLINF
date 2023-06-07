e8178fd632e842acabafa7750152a8df6981c3ed
maelget1 <112376326+maelget1@users.noreply.github.com>
2023-06-07 09:04:29 +0200

    finir de mettre le site en français

--- code/php/signIn.php
+++ code/php/signIn.php
@@ -12,7 +12,7 @@ session_start();
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Sign In Page</title>
+        <title>Page d'enregistrement</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/signIn.css">
     </head>
